9c05416d29612995bfc8c8fb165df96947f5aaa9
SiulRek <luis.kraker@edu.fh-joanneum.at>
2025-05-28 13:36:35 -0500

    Refactor local instance cleanup logic in run.py

--- src/run.py
+++ src/run.py
@@ -1,13 +1,14 @@
+import os
+
 from app import create_app
 
-# TODO: Remove this lines when the app is ready for production
-# # -----------------------
-# import shutil
-# import os
-# instance_path = os.path.join(os.getcwd(), "src", "instance")
-# if os.path.exists(instance_path):
-#     shutil.rmtree(instance_path)
-# # -----------------------
+if os.getenv("RUNNING_LOCALLY", "False") == "true":
+    import shutil
+
+    instance_path = os.path.join(os.getcwd(), "src", "instance")
+    if os.path.exists(instance_path):
+        shutil.rmtree(instance_path)
+
 
 app = create_app()
 
