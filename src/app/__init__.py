ff360806cea5a759e5a687cdbc412a6d13632f8c
SiulRek <luis.kraker@edu.fh-joanneum.at>
2025-05-29 08:20:41 -0500

    Refactor: extract app config to separate function

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate
 
 mail = Mail()
@@ -10,15 +11,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app():
-    app = Flask(
-        __name__,
-        template_folder=os.path.join(
-            os.path.dirname(__file__), "../templates"
-        ),
-        static_folder=os.path.join(os.path.dirname(__file__), "../static"),
-    )
-
+def configure_app(app):
     app.config["SECRET_KEY"] = "dev-secret"
 
     default_db = "sqlite:///app_database.db"
@@ -35,13 +28,23 @@ def create_app():
     app.config["MAIL_USERNAME"] = "luiskraker2000@gmail.com"
     app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
+
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(
+            os.path.dirname(__file__), "../templates"
+        ),
+        static_folder=os.path.join(os.path.dirname(__file__), "../static"),
+    )
+    configure_app(app)
+
     db.init_app(app)
     migrate.init_app(app, db)
 
     mail.init_app(app)
 
     with app.app_context():
-        from app.models import User, PendingUser
         from app.auth_service import AuthService
 
         app.auth = AuthService(db=db)
