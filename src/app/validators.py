ed6875c597fe1d1e993651e8b3696bc417613d52
SiulRek <luis.kraker@edu.fh-joanneum.at>
2025-05-29 13:17:32 -0500

    Refactor code to comply with PEP8 standards

--- src/app/validators.py
+++ src/app/validators.py
@@ -3,51 +3,31 @@ from app.models import User
 
 def assert_username(username):
     if not username:
-        raise AssertionError(
-            "Username cannot be empty."
-        )
+        raise AssertionError("Username cannot be empty.")
     if len(username) < 3:
-        raise AssertionError(
-            "Username must be at least 3 characters long."
-        )
+        raise AssertionError("Username must be at least 3 characters long.")
     if not username.isalnum():
-        raise AssertionError(
-            "Username must be alphanumeric."
-        )
+        raise AssertionError("Username must be alphanumeric.")
     if len(username) > 32:
-        raise AssertionError(
-            "Username cannot be longer than 32 characters."
-        )
+        raise AssertionError("Username cannot be longer than 32 characters.")
 
 
 def assert_email(email):
     if not email:
-        raise AssertionError(
-            "Email cannot be empty."
-        )
+        raise AssertionError("Email cannot be empty.")
     if "@" not in email or "." not in email:
-        raise AssertionError(
-            "Invalid email format."
-        )
+        raise AssertionError("Invalid email format.")
 
 
 def assert_password(password):
     if not password:
-        raise AssertionError(
-            "Password cannot be empty."
-        )
+        raise AssertionError("Password cannot be empty.")
     if len(password) < 6:
-        raise AssertionError(
-            "Password must be at least 6 characters long."
-        )
+        raise AssertionError("Password must be at least 6 characters long.")
     if not any(char.isdigit() for char in password):
-        raise AssertionError(
-            "Password must contain at least one digit."
-        )
+        raise AssertionError("Password must contain at least one digit.")
     if not any(char.isalpha() for char in password):
-        raise AssertionError(
-            "Password must contain at least one letter."
-        )
+        raise AssertionError("Password must contain at least one letter.")
     if not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>?/" for char in password):
         raise AssertionError(
             "Password must contain at least one special character."
@@ -58,6 +38,7 @@ def assert_password(password):
         raise AssertionError(
             "Password must contain at least one uppercase letter."
         )
+
 
 def assert_unique_username_and_email(username, email):
     existing_user = User.query.filter(
