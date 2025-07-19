81eccdf97c7f199a2b0eb61d19f05177432e7a9d
SiulRek <luis.kraker@edu.fh-joanneum.at>
2025-07-19 09:02:01 +0200

    Add MemoryQuizUnit to quiz unit mapping dictionary

--- src/quiz/units/__init__.py
+++ src/quiz/units/__init__.py
@@ -1,8 +1,10 @@
 from quiz.units.date_quiz_unit import DateQuizUnit
 from quiz.units.math_quiz_unit import MathQuizUnit
+from quiz.units.memory_quiz_unit import MemoryQuizUnit
 
 QUIZ_UNIT_MAPPING = {
     "date": DateQuizUnit,
-    "math": MathQuizUnit
+    "math": MathQuizUnit,
+    "memory": MemoryQuizUnit,
     # TODO: Add more quiz units as needed
 }
